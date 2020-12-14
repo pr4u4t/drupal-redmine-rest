60e19036eb2ca9fbb09ccf521e13af45e9c5a148
Kamila Klonowska <kamila.klonowska@robco.pl>
2020-12-14 13:04:45 +0100

    Added Help desk widget block

---
+++ src/Plugin/Block/HelpDesk.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\robco_rest\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Help Desk' Block.
+ *
+ * @Block(
+ *   id = "help_desk_block",
+ *   admin_label = @Translation("Help desk block"),
+ *   category = @Translation("Help Desk"),
+ * )
+ */
+class HelpDeskBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $tag = '<span>
+        <div id="helpdesk_widget"></div>
+        <script type="text/javascript" src="https://office.robco.pl/helpdesk_widget/widget.js"></script>
+      	</span>';
+    
+    	
+    return [
+      '#markup' => $tag,
+      '#allowed_tags' => ['script', 'div', 'span'],
+    ];
+  } 
+}
