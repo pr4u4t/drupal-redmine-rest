b21094efc5cbb63354c0066e9ec71b9c3b32b01b
Kamila Klonowska <kamila.klonowska@robco.pl>
2020-12-14 12:16:31 +0100

    Preparing add to cart and remove from cart view fields

---
+++ src/Plugin/views/field/AddToCart.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Drupal\robco_rest\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\ViewExecutable;
+
+/**
+ * A handler to provide a field that is completely custom by the administrator.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("add_to_cart_views_field")
+ */
+class AddToCartViewsField extends FieldPluginBase {
+
+  /**
+   * The current display.
+   *
+   * @var string
+   *   The current display of the view.
+   */
+  protected $currentDisplay;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->currentDisplay = $view->current_display;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function usesGroupBy() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Do nothing -- to override the parent query.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    
+    $options['alter']['contains']['alter_text'] = ['default' => TRUE];
+    $options['hide_alter_empty'] = ['default' => FALSE];
+    
+    return $options;
+  }
+
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    // Remove the checkbox
+    unset($form['alter']['alter_text']);
+    unset($form['alter']['text']['#states']);
+    unset($form['alter']['help']['#states']);
+    $form['#pre_render'][] = [$this, 'preRenderCustomForm'];
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public static function trustedCallbacks() {
+    $callbacks = parent::trustedCallbacks();
+    $callbacks[] = 'preRenderCustomForm';
+    return $callbacks;
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $node = $values->_entity;
+    $states = workflow_get_workflow_state_names();
+    return $states[$node->get('field_phase')->getValue()[0]['value']];
+  }
+
+  /**
+   * Prerender function to move the textarea to the top of a form.
+   *
+   * @param array $form
+   *   The form build array.
+   *
+   * @return array
+   *   The modified form build array.
+   */
+  public function preRenderCustomForm($form) {
+    $form['text'] = $form['alter']['text'];
+    $form['help'] = $form['alter']['help'];
+    unset($form['alter']['text']);
+    unset($form['alter']['help']);
+
+    return $form;
+  }
+}
