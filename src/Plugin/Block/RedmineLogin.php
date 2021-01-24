94637dbedcd30bb39ee5fe01b145128407fdc671
Kamila Klonowska <kamila.klonowska@robco.pl>
2021-01-24 23:29:41 +0100

    Login method update

--- src/Plugin/Block/RedmineLogin.php
+++ src/Plugin/Block/RedmineLogin.php
@@ -91,12 +91,12 @@ class RedmineLogin extends BlockBase {
                     <form class="needs-validation" novalidate="" method="post" action="/robco_rest/login">
                         <div class="input-group form-group">
                             <div class="input-group-prepend"><span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></span></div>
-                            <input class="form-control" type="email" placeholder="Login" required="">
+                            <input class="form-control" type="text" placeholder="Login" required="" name="login" id="login">
                             <div class="invalid-feedback">Please enter valid login!</div>
                         </div>
                         <div class="input-group form-group">
                             <div class="input-group-prepend"><span class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg></span></div>
-                            <input class="form-control" type="password" placeholder="Password" required="">
+                            <input class="form-control" type="password" placeholder="Password" required="" name="password" id="password">
                             <div class="invalid-feedback">Please enter valid password!</div>
                         </div>
                         <div class="d-flex flex-wrap justify-content-between">
